bb070eb55bc0d9f97ec7b9a641a21e4df39a9d96
Juan <juanben10xlr99@mail.com>
2023-08-22 12:56:07 +0100

    Base Recognition Model Done

--- run.py
+++ run.py
@@ -1,8 +1,43 @@
 import cv2
-import dlib
 
-cam = cv2.VideoCapture('video.mp4')
+# Load the pre-trained classifiers for face and eyes
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-while 1:
-    print(cam)
-    break
+# Open a video capture stream (0 is usually the default webcam)
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read a frame from the video stream
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale for better detection performance
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in faces:
+        # Draw a rectangle around the detected face
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # Extract the region of interest for face detection (within the face rectangle)
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        # Detect eyes within the region of interest (face)
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (ex, ey, ew, eh) in eyes:
+            # Draw rectangles around the detected eyes (within the face rectangle)
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Video', frame)
+
+    # Break the loop if 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the capture stream and close all OpenCV windows
+cap.release()
+cv2.destroyAllWindows()
